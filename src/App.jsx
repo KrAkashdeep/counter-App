9c3c226c415aae776bef5cf67cffe228c614db94
KrAkashdeep <akashdeep93140@gmail.com>
2024-01-26 17:30:40 +0530

    initial files

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function App() {
+  return (
+    <div>
+      <h1>1</h1>
+      <button>increase</button>
+      <button>decrease</button>
+    </div>
+  );
+}
+
+export default App;
