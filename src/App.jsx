2b56fe958e08939b43477cce1159c5195fab4601
KrAkashdeep <akashdeep93140@gmail.com>
2024-01-26 18:11:21 +0530

    add app file

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
+import style from "./app.module.css";
 
 function App() {
+  const [num, setnum] = useState(0);
+
+  function inc() {
+    setnum(num + 1);
+  }
+  function dec() {
+    setnum(num - 1);
+  }
   return (
-    <div>
-      <h1>1</h1>
-      <button>increase</button>
-      <button>decrease</button>
-    </div>
+    <section className={style.sect}>
+      <div className={style.container}>
+        <div className={style.insidebox}>
+          <h1>{num}</h1>
+        </div>
+
+        <div className={style.butbox}>
+          <button className={style.button1} onClick={dec}>
+            decrease
+          </button>
+        </div>
+        <div className={style.butbox1}>
+          <button className={style.button2} onClick={inc}>
+            increase
+          </button>
+        </div>
+      </div>
+    </section>
   );
 }
 
